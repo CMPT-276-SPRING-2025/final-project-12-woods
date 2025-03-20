51f870d9a042bd24e1180c14207e035326f256f2
Amir Matianiu <70409945+Amik05@users.noreply.github.com>
2025-03-19 22:41:46 -0700

    Added an image carousel & maps to home page (#40)
    * Added googlemaps feature to frontpage
    
    added drop pin and interactive features
    
    * fix: import correct dependencies
    
    ---------
    
    Co-authored-by: damonyiu <150401782+damonyiu@users.noreply.github.com>

--- foodiefind/src/pages/Home.jsx
+++ foodiefind/src/pages/Home.jsx
@@ -1,10 +1,84 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import photo1 from '/src/assets/images/photo1.jpg';
 import photo2 from '/src/assets/images/photo2.jpg';
 import photo3 from '/src/assets/images/photo3.jpg';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
 
+//adding map function
+function loadGoogleMapsAPI() {
+  const script = document.createElement("script");
+  script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAZbkQiYGY81HUVJ4dgX9YJZAr_kS_Lvfo&callback=initMap`;
+  script.async = true;
+  document.head.appendChild(script);
+
+  window.initMap = function () {
+    const map = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: 49.2827, lng: -123.1207 },
+      zoom: 10,
+    });
+
+    let infoWindow = null;
+
+    // Load saved markers from local storage
+    const savedMarkers = JSON.parse(localStorage.getItem("markers")) || [];
+    savedMarkers.forEach(({ position, title }) => {
+      const marker = new window.google.maps.Marker({
+        position,
+        map,
+        title,
+      });
+
+      const infoWindow = new window.google.maps.InfoWindow({
+        content: `<div><h3>${title}</h3><p>Additional information can go here.</p></div>`,
+      });
+
+      marker.addListener("click", () => {
+        infoWindow.open(map, marker);
+      });
+    });
+
+    map.addListener("click", (event) => {
+      const placeName = prompt("Enter the name of the place:");
+      if (placeName) {
+        const marker = new window.google.maps.Marker({
+          position: event.latLng,
+          map: map,
+          title: placeName,
+        });
+
+        infoWindow = new window.google.maps.InfoWindow({
+          content: `<div><h3>${placeName}</h3><p>Additional information can go here.</p></div>`,
+        });
+
+        marker.addListener("click", () => {
+          infoWindow.open(map, marker);
+        });
+
+        // Save marker to local storage
+        savedMarkers.push({
+          position: event.latLng.toJSON(),
+          title: placeName,
+        });
+        localStorage.setItem("markers", JSON.stringify(savedMarkers));
+      }
+    });
+
+    map.addListener("click", () => {
+      if (infoWindow) {
+        infoWindow.close();
+      }
+    });
+  };
+
+  return () => {
+    document.head.removeChild(script);
+    delete window.initMap;
+  };
+}
+
+//funtionality for slideshow
 function Home() {
   const slides = [
     { url: photo1 },
@@ -30,6 +104,12 @@ function Home() {
     setCurrentIndex(slideIndex);
   };
 
+  //useffect for googlemaps
+  useEffect(() => {
+    const cleanup = loadGoogleMapsAPI();
+    return cleanup;
+  }, []);
+  
   return (
     <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
       {/* Background image */}
@@ -69,6 +149,7 @@ function Home() {
 
       <h1>Home Page</h1>
       <p>Welcome to FoodieFind!</p>
+      <div id="map" style={{ height: "600px", width: "100%" }}></div>
     </div>
   );
 }
