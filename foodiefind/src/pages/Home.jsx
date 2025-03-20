8d2c7f14ff9b02625150bf9859432fc528955235
Jim Chen <jimcnerf@gmail.com>
2025-03-19 18:24:34 -0700

    Added slideshow gallery and removed index css
    index css was removed to prevent css conflict and slideshow was added from front end

--- foodiefind/src/pages/Home.jsx
+++ foodiefind/src/pages/Home.jsx
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import photo1 from '/src/assets/images/photo1.jpg';
+import photo2 from '/src/assets/images/photo2.jpg';
+import photo3 from '/src/assets/images/photo3.jpg';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
 
 function Home() {
+  const slides = [
+    { url: photo1 },
+    { url: photo2 },
+    { url: photo3 }
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
   return (
-    <div>
+    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+      {/* Background image */}
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+      ></div>
+
+      {/* Left Arrow */}
+      <div
+        onClick={prevSlide}
+        className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'
+      >
+        <BsChevronCompactLeft size={30} />
+      </div>
+
+      {/* Right Arrow */}
+      <div
+        onClick={nextSlide}
+        className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'
+      >
+        <BsChevronCompactRight size={30} />
+      </div>
+
+      {/* Dots Navigation */}
+      <div className='flex justify-center py-2'>
+        {slides.map((_, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className={`text-2xl cursor-pointer ${currentIndex === slideIndex ? 'text-blue-500' : 'text-gray-500'}`}
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+
       <h1>Home Page</h1>
       <p>Welcome to FoodieFind!</p>
     </div>
