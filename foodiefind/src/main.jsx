ff2b54365254a884843e24778d77638f5168e57c
Amik05 <amatianiu@gmail.com>
2025-04-06 15:34:43 -0700

    fix: remove legacy warning in production code

--- foodiefind/src/main.jsx
+++ foodiefind/src/main.jsx
@@ -1,12 +1,14 @@
 // main.jsx
 
-// Suppress Google Maps Legacy API warning
+// Suppress Google Maps Legacy API warnings in production code
 // This is a workaround, since they have chaged the Places API on March 1, 2025
-console.log = () => {};
-console.info = () => {};
-console.warn = () => {};
-console.error = () => {};
-
+// Only disable console logs in production
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
